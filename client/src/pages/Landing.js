3b89290de36c230bd875b0538812880c4c41b3a9
Bolatbekermekov <132038849+Bolatbekermekov@users.noreply.github.com>
2024-01-10 23:34:39 +0600

    first commit

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,30 @@
+import main from '../assets/images/main.svg';
+import Wrapper from '../assets/wrappers/LandingPage';
+import { Logo } from '../components';
+import { Link } from 'react-router-dom';
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className='container page'>
+        {/* info */}
+        <div className='info'>
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+          Hello! I'm Bolatbek Ermekov, the creator of this MERN project. This platform is a comprehensive web application designed for managing job vacancies and user interactions. It includes user registration, login functionality, job creation, updating job details, and accessing a list of all available jobs.
+          </p>
+          <Link to='/register' className='btn btn-hero'>
+            Login/Register
+          </Link>
+        </div>
+        <img src={main} alt='job hunt' className='img main-img' />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Landing;
